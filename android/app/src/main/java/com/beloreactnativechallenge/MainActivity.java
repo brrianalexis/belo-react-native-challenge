3f0846f8dfa550fd958bbbe53e615250995d23ca
Brian Alexis Michel <51463930+brrianalexis@users.noreply.github.com>
2022-05-20 03:31:13 -0300

    feat: device navigation config

--- android/app/src/main/java/com/beloreactnativechallenge/MainActivity.java
+++ android/app/src/main/java/com/beloreactnativechallenge/MainActivity.java
@@ -1,10 +1,19 @@
 package com.beloreactnativechallenge;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  /**
+   * react-navigation config
+  */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
